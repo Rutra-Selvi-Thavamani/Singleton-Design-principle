6314a412252dbf59501de479deccab62443fe191
Rutra-Selvi <RutraSelvi.Net@gmail.com>
2022-11-25 11:23:13 +0530

    my singleton exxample

---
+++ Singleton Design Pattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Design_Pattern
+{
+    public class Student
+    {
+        static void Main(string[] args)
+        {
+            School student = School.GetInstance();   //method invoking
+            School studentsecond = School.GetInstance();
+            string sStudentName = student.getStudentDetails();
+            string sStudentSecondName = studentsecond.getStudentSecondDetails();
+            Console.WriteLine(sStudentName);
+            Console.WriteLine(sStudentSecondName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton Design Pattern/School.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Design_Pattern
+{
+    public class School
+    {
+        private static int counter = 0;
+        private School()
+        {
+            counter++;
+            Console.WriteLine("Counter Value:" + counter.ToString());
+        }
+        public static School objInstance;
+        private static readonly object mylockobject = new object();  //multi threaded
+        public static School GetInstance()
+        {
+            lock (mylockobject)
+            {
+                if (objInstance == null)
+                   objInstance = new School();
+            }
+            return objInstance;
+        }
+        public string getStudentDetails()
+        {
+            return "The First student name is - Aravind";
+        }
+        public string getStudentSecondDetails()
+        {
+            return "The seccond student is - Donald";
+        }
+    }
+}
+
